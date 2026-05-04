b55c5bc37e1295fce66c57384fa8871047af34b8
Patrick Fedick <patrick@pfp.de>
2026-05-04 23:04:36 +0200

    st7789 WIP

---
+++ include/st7789.h
@@ -0,0 +1,73 @@
+#ifndef ST7789_H
+#define ST7789_H
+#pragma once
+
+#include "hardware/dma.h"
+#include "hardware/spi.h"
+#include "pico/binary_info.h"
+#include "pico/stdlib.h"
+#include "picopplib.h"
+#include "picopplib-grafix.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+class ST7789
+{
+public:
+    class Config
+    {
+    public:
+        uint8_t pin_spi_dc;
+        uint8_t pin_spi_cs;
+        uint8_t pin_spi_rst;
+        uint8_t pin_spi_sck;
+        uint8_t pin_spi_data;
+        uint32_t pin_spi_speed;
+        uint8_t pin_spi_blk;
+
+        spi_inst_t* spi_num;
+    };
+
+private:
+    uint8_t* oled_dma[2];
+    uint8_t current_buffer;
+    size_t buffer_size;
+    uint16_t width;
+    uint16_t height;
+
+    uint8_t spi_dc;
+    uint8_t spi_cs;
+    uint8_t spi_rst;
+    uint8_t spi_sck;
+    uint8_t spi_data;
+    uint32_t spi_speed;
+    uint8_t spi_blk;
+    spi_inst_t* spi_num;
+
+    volatile unsigned int dma_tx;
+    dma_channel_config config;
+
+    void write(const uint8_t cmd, const uint8_t* data, size_t len);
+    void flush_dma(uint8_t* ptr, size_t len);
+    void oled_init();
+
+public:
+    ST7789();
+    ~ST7789();
+
+    void init(uint16_t width, uint16_t height, const Config& config);
+
+    uint8_t* get_buffer();
+
+    void refresh();
+    picopplib::Drawable getDrawable();
+
+    void clear(uint16_t color = 0x0000);
+    void putPixel(int x, int y, uint16_t color);
+    void fillRect(int x1, int y1, int x2, int y2, uint16_t color);
+    void drawRect(int x1, int y1, int x2, int y2, uint16_t color);
+    void line(int x1, int y1, int x2, int y2, uint16_t color);
+};
+
+#endif // ST7789_H
---
+++ src/hardware/st7789.cpp
@@ -0,0 +1,512 @@
+#include "st7789.h"
+#include <malloc.h>
+#include <string.h>
+
+typedef enum write_type
+{
+    WRITE_COMMAND = 0,
+    WRITE_DATA = 1
+} write_type;
+
+typedef enum st7789_commands
+{
+    // System Function Commands
+    CMD_NOP = 0x00,       // No Operation
+    CMD_SWRESET = 0x01,   // Software Reset
+    CMD_RDDID = 0x04,     // Read Display ID
+    CMD_RDDST = 0x09,     // Read Display Status
+    CMD_RDDPM = 0x0A,     // Read Display Power Mode
+    CMD_RDDMADCTL = 0x0B, // Read Display MADCTL
+    CMD_RDDCOLMOD = 0x0C, // Read Display Pixel Format
+    CMD_RDDIM = 0x0D,     // Read Display Image Mode
+    CMD_RDDSM = 0x0E,     // Read Display Signal Mode
+    CMD_RDDSDR = 0x0F,    // Read Display Self-Diagnostic Result
+
+    // Sleep Mode Commands
+    CMD_SLPIN = 0x10,  // Sleep In (enter sleep mode)
+    CMD_SLPOUT = 0x11, // Sleep Out (exit sleep mode)
+    CMD_PTLON = 0x12,  // Partial Display Mode On
+    CMD_NORON = 0x13,  // Normal Display Mode On
+
+    // Display Inversion Commands
+    CMD_INVOFF = 0x20, // Display Inversion Off
+    CMD_INVON = 0x21,  // Display Inversion On
+
+    // Display On/Off Commands
+    CMD_DISPOFF = 0x28, // Display Off
+    CMD_DISPON = 0x29,  // Display On
+
+    // Address Setting Commands
+    CMD_CASET = 0x2A, // Column Address Set
+    CMD_RASET = 0x2B, // Row Address Set
+    CMD_RAMWR = 0x2C, // Memory Write
+    CMD_RAMRD = 0x2E, // Memory Read
+
+    // Partial Area Commands
+    CMD_PTLAR = 0x30,   // Partial Area (define partial display area)
+    CMD_VSCRDEF = 0x33, // Vertical Scrolling Definition
+    CMD_TEOFF = 0x34,   // Tearing Effect Line OFF
+    CMD_TEON = 0x35,    // Tearing Effect Line ON
+
+    // Memory Access Control
+    CMD_MADCTL = 0x36,   // Memory Data Access Control (rotation/mirroring)
+    CMD_VSCRSADD = 0x37, // Vertical Scrolling Start Address
+    CMD_IDMOFF = 0x38,   // Idle Mode Off
+    CMD_IDMON = 0x39,    // Idle Mode On
+
+    // Pixel Format
+    CMD_COLMOD = 0x3A, // Interface Pixel Format (color depth: 12/16/18 bit)
+    CMD_RAMWRC = 0x3C, // Memory Write Continue
+    CMD_RAMRDC = 0x3E, // Memory Read Continue
+
+    // Tearing Effect
+    CMD_TESCAN = 0x44,   // Set Tear Scanline
+    CMD_RDTESCAN = 0x45, // Get Scanline
+
+    // Brightness Control
+    CMD_WRDISBV = 0x51,  // Write Display Brightness
+    CMD_RDDISBV = 0x52,  // Read Display Brightness Value
+    CMD_WRCTRLD = 0x53,  // Write CTRL Display
+    CMD_RDCTRLD = 0x54,  // Read CTRL Value Display
+    CMD_WRCACE = 0x55,   // Write Content Adaptive Brightness Control and Color Enhancement
+    CMD_RDCABC = 0x56,   // Read Content Adaptive Brightness Control
+    CMD_WRCABCMB = 0x5E, // Write CABC Minimum Brightness
+    CMD_RDCABCMB = 0x5F, // Read CABC Minimum Brightness
+
+    // RGB Interface Control
+    CMD_RAMCTRL = 0xB0, // RAM Control
+    CMD_RGBCTRL = 0xB1, // RGB Interface Control
+    CMD_PORCTRL = 0xB2, // Porch Setting (front/back porch control)
+    CMD_FRCTRL1 = 0xB3, // Frame Rate Control 1 (in partial mode/idle colors)
+
+    // Gate Control
+    CMD_GCTRL = 0xB7, // Gate Control
+    CMD_GTADJ = 0xB8, // Gate On Timing Adjustment
+    CMD_DGMEN = 0xBA, // Digital Gamma Enable
+
+    // VCOM Setting
+    CMD_VCOMS = 0xBB, // VCOM Setting
+
+    // Power Control
+    CMD_LCMCTRL = 0xC0,  // LCM Control
+    CMD_IDSET = 0xC1,    // ID Code Setting
+    CMD_VDVVRHEN = 0xC2, // VDV and VRH Command Enable
+    CMD_VRHS = 0xC3,     // VRH Set (voltage setting)
+    CMD_VDVS = 0xC4,     // VDV Set (voltage setting)
+    CMD_VCMOFSET = 0xC5, // VCOM Offset Set
+    CMD_FRCTRL2 = 0xC6,  // Frame Rate Control in Normal Mode
+    CMD_CABCCTRL = 0xC7, // CABC Control
+    CMD_REGSEL1 = 0xC8,  // Register Value Selection 1
+    CMD_REGSEL2 = 0xCA,  // Register Value Selection 2
+    CMD_PWMFRSEL = 0xCC, // PWM Frequency Selection
+
+    // Power Control
+    CMD_PWCTRL1 = 0xD0,  // Power Control 1
+    CMD_VAPVANEN = 0xD2, // Enable VAP/VAN signal output
+    CMD_RDID1 = 0xDA,    // Read ID1
+    CMD_RDID2 = 0xDB,    // Read ID2
+    CMD_RDID3 = 0xDC,    // Read ID3
+
+    // Gamma Control
+    CMD_PVGAMCTRL = 0xE0, // Positive Voltage Gamma Control
+    CMD_NVGAMCTRL = 0xE1, // Negative Voltage Gamma Control
+    CMD_DGMLUTR = 0xE2,   // Digital Gamma Look-up Table for Red
+    CMD_DGMLUTB = 0xE3,   // Digital Gamma Look-up Table for Blue
+
+    // Other Commands
+    CMD_GATECTRL = 0xE4, // Gate Control
+    CMD_SPI2EN = 0xE7,   // SPI2 Enable
+    CMD_PWCTRL2 = 0xE8,  // Power Control 2
+    CMD_EQCTRL = 0xE9,   // Equalize time control
+    CMD_PROMCTRL = 0xEC, // Program Mode Control
+    CMD_PROMEN = 0xFA,   // Program Mode Enable
+    CMD_NVMSET = 0xFC,   // NVM Setting
+    CMD_PROMACT = 0xFE   // Program action
+
+} st7789_commands;
+
+ST7789::ST7789()
+{
+    width = 0;
+    height = 0;
+    buffer_size = 0;
+    oled_dma[0] = nullptr;
+    oled_dma[1] = nullptr;
+    current_buffer = 0;
+    spi_dc = -1;
+    spi_cs = -1;
+    spi_rst = -1;
+    spi_sck = -1;
+    spi_data = -1;
+    spi_blk = -1;
+    spi_speed = 20000000;
+    spi_num = nullptr;
+    dma_tx = 0;
+}
+
+ST7789::~ST7789()
+{
+    free(oled_dma[0]);
+    free(oled_dma[1]);
+}
+
+void ST7789::init(uint16_t width, uint16_t height, const Config& config)
+{
+    this->width = width;
+    this->height = height;
+    buffer_size = width * height * 2;
+    oled_dma[0] = (uint8_t*)malloc(buffer_size);
+    oled_dma[1] = (uint8_t*)malloc(buffer_size);
+    current_buffer = 0;
+    if (!oled_dma[0] || !oled_dma[1]) {
+        return;
+    }
+    memset(oled_dma[0], 0, buffer_size);
+    memset(oled_dma[1], 0, buffer_size);
+
+    spi_dc = config.pin_spi_dc;
+    spi_cs = config.pin_spi_cs;
+    spi_rst = config.pin_spi_rst;
+    spi_rst = config.pin_spi_rst;
+    spi_sck = config.pin_spi_sck;
+    spi_data = config.pin_spi_data;
+    spi_speed = config.pin_spi_speed;
+    spi_blk = config.pin_spi_blk;
+    spi_num = config.spi_num;
+    oled_init();
+}
+
+void ST7789::write(const uint8_t cmd, const uint8_t* data, size_t len)
+{
+    gpio_put(spi_cs, 0); // CS aktivieren (LOW)
+    gpio_put(spi_dc, WRITE_COMMAND);
+    spi_write_blocking(spi_num, &cmd, 1);
+    if (len) {
+        gpio_put(spi_dc, WRITE_DATA);
+        spi_write_blocking(spi_num, data, len);
+    }
+    gpio_put(spi_cs, 1); // CS deaktivieren (HIGH)
+}
+
+void ST7789::flush_dma(uint8_t* ptr, size_t len)
+{
+    dma_channel_wait_for_finish_blocking(dma_tx);
+
+    uint8_t param[4];
+
+    gpio_put(spi_cs, 0); // CS aktivieren für gesamte Sequenz
+
+    // Column Address Set (CASET) - 0 bis 239
+    param[0] = 0x00;               // Start MSB
+    param[1] = 0x00;               // Start LSB (Spalte 0)
+    param[2] = (width - 1) >> 8;   // End MSB
+    param[3] = (width - 1) & 0xFF; // End LSB (Spalte 239)
+    gpio_put(spi_dc, WRITE_COMMAND);
+    spi_write_blocking(spi_num, (const uint8_t[]){CMD_CASET}, 1);
+    gpio_put(spi_dc, WRITE_DATA);
+    spi_write_blocking(spi_num, param, 4);
+
+    // Row Address Set (RASET) - 0 bis 239
+    param[0] = 0x00;                // Start MSB
+    param[1] = 0x00;                // Start LSB (Zeile 0)
+    param[2] = (height - 1) >> 8;   // End MSB
+    param[3] = (height - 1) & 0xFF; // End LSB (Zeile 239)
+    gpio_put(spi_dc, WRITE_COMMAND);
+    spi_write_blocking(spi_num, (const uint8_t[]){CMD_RASET}, 1);
+    gpio_put(spi_dc, WRITE_DATA);
+    spi_write_blocking(spi_num, param, 4);
+
+    // Memory Write
+    gpio_put(spi_dc, WRITE_COMMAND);
+    spi_write_blocking(spi_num, (const uint8_t[]){CMD_RAMWR}, 1);
+    gpio_put(spi_dc, WRITE_DATA);
+
+    // DMA Transfer
+    dma_channel_transfer_from_buffer_now(dma_tx, ptr, len);
+    dma_channel_wait_for_finish_blocking(dma_tx);
+
+    gpio_put(spi_cs, 1); // CS deaktivieren nach Transfer
+}
+
+uint8_t* ST7789::get_buffer()
+{
+    return oled_dma[current_buffer];
+}
+
+picopplib::Drawable ST7789::getDrawable()
+{
+    return picopplib::Drawable(oled_dma[current_buffer], width * 2, width, height, picopplib::RGBFormat::R5G6B5);
+}
+
+void ST7789::oled_init()
+{
+    uint8_t param[14]; // Größeres Array für längere Parameterlisten
+
+    // Setup SPI - WICHTIG: ST7789 benötigt CPOL_0 und CPHA_0!
+    spi_init(spi_num, spi_speed);
+    spi_set_format(spi_num, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
+    gpio_set_function(spi_sck, GPIO_FUNC_SPI);
+    gpio_set_function(spi_data, GPIO_FUNC_SPI);
+
+    gpio_init(spi_cs);
+    gpio_set_dir(spi_cs, GPIO_OUT);
+    gpio_put(spi_cs, 0); // CS aktiv (LOW)
+
+    // Setup GPIO Pins
+    gpio_init(spi_dc);
+    gpio_set_dir(spi_dc, GPIO_OUT);
+    gpio_put(spi_dc, 1);
+
+    gpio_init(spi_rst);
+    gpio_set_dir(spi_rst, GPIO_OUT);
+
+    // Backlight Pin initialisieren und einschalten
+    gpio_init(spi_blk);
+    gpio_set_dir(spi_blk, GPIO_OUT);
+    gpio_put(spi_blk, 1); // Backlight an
+
+    // Hardware Reset
+    gpio_put(spi_rst, 1);
+    sleep_ms(10);
+    gpio_put(spi_rst, 0);
+    sleep_ms(10);
+    gpio_put(spi_rst, 1);
+    sleep_ms(120); // Warten bis Display bereit ist
+
+    // Software Reset
+    write(CMD_SWRESET, NULL, 0);
+    sleep_ms(150); // WICHTIG: Nach Software-Reset warten!
+
+    // Sleep Out (Display aufwecken)
+    write(CMD_SLPOUT, NULL, 0);
+    sleep_ms(120); // WICHTIG: Nach SLPOUT warten!
+
+    // Interface Pixel Format: 16-bit/pixel (RGB565)
+    param[0] = 0x55; // 0x55 = 16-bit RGB565
+    write(CMD_COLMOD, param, 1);
+
+    // Memory Data Access Control (Display-Orientierung)
+    // MY=0, MX=0, MV=0, ML=0, RGB=1, MH=0
+    param[0] = 0x00; // 0x00 = Standard-Orientierung
+    // Alternative Werte:
+    // 0x00 = 0°   (Portrait)
+    // 0x60 = 90°  (Landscape)
+    // 0xC0 = 180° (Portrait, gespiegelt)
+    // 0xA0 = 270° (Landscape, gespiegelt)
+    write(CMD_MADCTL, param, 1);
+
+    // Porch Setting (Front und Back Porch)
+    param[0] = 0x0C; // Back porch in normal mode
+    param[1] = 0x0C; // Front porch in normal mode
+    param[2] = 0x00; // Disable separate porch control
+    param[3] = 0x33; // Back porch in idle mode
+    param[4] = 0x33; // Front porch in idle mode
+    write(CMD_PORCTRL, param, 5);
+
+    // Gate Control
+    param[0] = 0x35; // Default value
+    write(CMD_GCTRL, param, 1);
+
+    // VCOM Setting
+    param[0] = 0x19; // VCOM = 0.725V (typischer Wert)
+    write(CMD_VCOMS, param, 1);
+
+    // LCM Control
+    param[0] = 0x2C;
+    write(CMD_LCMCTRL, param, 1);
+
+    // VDV and VRH Command Enable
+    param[0] = 0x01; // Enable command 2
+    write(CMD_VDVVRHEN, param, 1);
+
+    // VRH Set (GVDD voltage setting)
+    param[0] = 0x12; // 4.45V + (vcom+vcom offset+vdv)
+    write(CMD_VRHS, param, 1);
+
+    // VDV Set
+    param[0] = 0x20; // 0V
+    write(CMD_VDVS, param, 1);
+
+    // Frame Rate Control in Normal Mode
+    param[0] = 0x0F; // 60 Hz (standard frame rate)
+    write(CMD_FRCTRL2, param, 1);
+
+    // Power Control 1
+    param[0] = 0xA4; // AVDD=6.8V, AVCL=-4.8V, VDDS=2.3V
+    param[1] = 0xA1; // VGH=14.06V, VGL=-9.32V
+    write(CMD_PWCTRL1, param, 2);
+
+    // Positive Voltage Gamma Control
+    param[0] = 0xD0;
+    param[1] = 0x04;
+    param[2] = 0x0D;
+    param[3] = 0x11;
+    param[4] = 0x13;
+    param[5] = 0x2B;
+    param[6] = 0x3F;
+    param[7] = 0x54;
+    param[8] = 0x4C;
+    param[9] = 0x18;
+    param[10] = 0x0D;
+    param[11] = 0x0B;
+    param[12] = 0x1F;
+    param[13] = 0x23;
+    write(CMD_PVGAMCTRL, param, 14);
+
+    // Negative Voltage Gamma Control
+    param[0] = 0xD0;
+    param[1] = 0x04;
+    param[2] = 0x0C;
+    param[3] = 0x11;
+    param[4] = 0x13;
+    param[5] = 0x2C;
+    param[6] = 0x3F;
+    param[7] = 0x44;
+    param[8] = 0x51;
+    param[9] = 0x2F;
+    param[10] = 0x1F;
+    param[11] = 0x1F;
+    param[12] = 0x20;
+    param[13] = 0x23;
+    write(CMD_NVGAMCTRL, param, 14);
+
+    // Display Inversion On (oft für bessere Farben nötig)
+    write(CMD_INVON, NULL, 0);
+
+    // Normal Display Mode On
+    write(CMD_NORON, NULL, 0);
+    sleep_ms(10);
+
+    // Display On
+    write(CMD_DISPON, NULL, 0);
+    sleep_ms(120);
+
+    // Setup DMA transfers
+    dma_tx = dma_claim_unused_channel(true);
+    config = dma_channel_get_default_config(dma_tx);
+    channel_config_set_transfer_data_size(&config, DMA_SIZE_8);
+    channel_config_set_dreq(&config, spi_get_index(spi_num) ? DREQ_SPI1_TX : DREQ_SPI0_TX);
+    dma_channel_configure(dma_tx, &config, &spi_get_hw(spi_num)->dr, oled_dma[0], buffer_size, false);
+}
+
+void ST7789::clear(uint16_t color)
+{
+    uint16_t* buffer = (uint16_t*)get_buffer();
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            buffer[y * width + x] = color;
+        }
+    }
+}
+
+void ST7789::refresh()
+{
+    if (current_buffer == 0) {
+        flush_dma(oled_dma[0], buffer_size);
+        // memcpy(oled_dma[1], oled_dma[0], buffer_size);
+        current_buffer = 1;
+    } else {
+        flush_dma(oled_dma[1], buffer_size);
+        // memcpy(oled_dma[0], oled_dma[1], buffer_size);
+        current_buffer = 0;
+    }
+}
+
+void ST7789::putPixel(int x, int y, uint16_t color)
+{
+    uint16_t* buffer = (uint16_t*)get_buffer();
+    if (x < 0 || x >= width || y < 0 || y >= height) return;
+    buffer[y * width + x] = color;
+}
+
+void ST7789::fillRect(int x1, int y1, int x2, int y2, uint16_t color)
+{
+    uint16_t* buffer = (uint16_t*)get_buffer();
+    for (int y = y1; y < y2; y++) {
+        for (int x = x1; x < x2; x++) {
+            putPixel(x, y, color);
+        }
+    }
+}
+
+void ST7789::drawRect(int x1, int y1, int x2, int y2, uint16_t color)
+{
+    uint16_t* buffer = (uint16_t*)get_buffer();
+    for (int y = y1; y < y2; y++) {
+        putPixel(x1, y, color);
+        putPixel(x2, y, color);
+    }
+    for (int x = x1; x < x2; x++) {
+        putPixel(x, y1, color);
+        putPixel(x, y2, color);
+    }
+}
+
+static inline int sgn(int x)
+{
+    return (x > 0) ? 1 : (x < 0) ? -1 : 0;
+}
+
+void ST7789::line(int x1, int y1, int x2, int y2, uint16_t color)
+{
+    // uint16_t *buffer = (uint16_t *) get_buffer();
+
+    /* Bresenham Algorithmus */
+    // static void Line_32 (DRAWABLE_DATA &data, int xstart, int ystart, int xend, int yend, SurfaceColor color)
+
+    int x, y, t, dx, dy, incx, incy, pdx, pdy, ddx, ddy, es, el, err;
+
+    /* Entfernung in beiden Dimensionen berechnen */
+    dx = x2 - x1;
+    dy = y2 - y1;
+
+    /* Vorzeichen des Inkrements bestimmen */
+    incx = sgn(dx);
+    incy = sgn(dy);
+    if (dx < 0) dx = -dx;
+    if (dy < 0) dy = -dy;
+
+    /* feststellen, welche Entfernung größer ist */
+    if (dx > dy) {
+        /* x ist schnelle Richtung */
+        pdx = incx;
+        pdy = 0; /* pd. ist Parallelschritt */
+        ddx = incx;
+        ddy = incy; /* dd. ist Diagonalschritt */
+        es = dy;
+        el = dx; /* Fehlerschritte schnell, langsam */
+    } else {
+        /* y ist schnelle Richtung */
+        pdx = 0;
+        pdy = incy; /* pd. ist Parallelschritt */
+        ddx = incx;
+        ddy = incy; /* dd. ist Diagonalschritt */
+        es = dx;
+        el = dy; /* Fehlerschritte schnell, langsam */
+    }
+
+    /* Initialisierungen vor Schleifenbeginn */
+    x = x1;
+    y = y1;
+    err = el / 2;
+    putPixel(x, y, color);
+
+    /* Pixel berechnen */
+    for (t = 0; t < el; ++t) /* t zaehlt die Pixel, el ist auch Anzahl */
+    {
+        /* Aktualisierung Fehlerterm */
+        err -= es;
+        if (err < 0) {
+            /* Fehlerterm wieder positiv (>=0) machen */
+            err += el;
+            /* Schritt in langsame Richtung, Diagonalschritt */
+            x += ddx;
+            y += ddy;
+        } else {
+            /* Schritt in schnelle Richtung, Parallelschritt */
+            x += pdx;
+            y += pdy;
+        }
+        putPixel(x, y, color);
+    }
+}
